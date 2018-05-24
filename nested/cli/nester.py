d474916fd50959874686341a9b24ca9be256893e
rlapar <laparradovan@gmail.com>
2018-05-24 19:00:48 +0200

    Nester CLI fix

--- nested/cli/nester.py
+++ nested/cli/nester.py
@@ -12,11 +12,12 @@ from nested.output.sketcher import Sketcher
 
 @click.command()
 @click.argument('input_fasta', required=True, type=click.Path(exists=True))
-@click.option('--sketch_only', '-s', is_flag=True, help='If true, nesting is not computed. Genes are sketched only from existing gff files.')
-@click.option('--format', '-f', default='default', help='Format for GFF.')
+@click.option('--sketch', help='Sketch output.', is_flag=True, default=False)
+#@click.option('--sketch_only', '-s', is_flag=True, help='If true, nesting is not computed. Genes are sketched only from existing gff files.')
+@click.option('--format', '-f', default='genome_browser', help='Format for GFF.')
 @click.option('--data_folder', '-d', type=str, help='Output data folder.')
 #TODO DATA_FOLDER
-def main(input_fasta, sketch_only, format, data_folder):
+def main(input_fasta, sketch, format, data_folder):
     number_of_errors = 0
     start_time = datetime.now()
     sequences = list(SeqIO.parse(open(input_fasta), 'fasta'))
@@ -27,12 +28,12 @@ def main(input_fasta, sketch_only, format, data_folder):
         strlen = 15
         print('Processing {a}...'.format(a=sequence.id[:strlen]), end='\r')
         try:
-            if not sketch_only:
-                nester = Nester(sequence)
-                sketcher.create_gff(nester.nested_element, format=format)
-                sketcher.create_gff(nester.nested_element)
-            
-            sketcher.sketch(sequence.id)
+            nester = Nester(sequence)
+            sketcher.create_gff(nester.nested_element, format=format)
+            if sketch:
+                if format != 'default':
+                    sketcher.create_gff(nester.nested_element)
+                sketcher.sketch(sequence.id)
             seq_end_time = datetime.now()
             print('Processing {a}: DONE [{b}]'.format(a=sequence.id[:strlen], b=seq_end_time - seq_start_time)) 
         except KeyboardInterrupt:
